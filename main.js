45daa1c1a51d5e2d2a18a2a5823cd3e43b2f750a
Christian Araujo <christianbehlingaraujo@gmail.com>
2023-05-31 08:07:18 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const btns = Array.from(document.querySelectorAll('button'))
+const toastLiveExample = document.getElementById('liveToast')
+const toast = new bootstrap.Toast(toastLiveExample)
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.dataset.content);
+        navigator.clipboard.writeText(btn.dataset.content);
+        toast.show()
+    })
+})
